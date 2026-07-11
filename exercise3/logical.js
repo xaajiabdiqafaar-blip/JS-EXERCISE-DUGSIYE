6fd8e359610ad28723f28c13f272882e357719f3
xaajiabdiqafaar-blip <xaajiabdiqafaar@gmail.com>
2026-07-11 17:49:18 +0300

    exercise4

--- exercise3/logical.js
+++ exercise3/logical.js
@@ -1,8 +1,24 @@
-let is_sunny = true;
-let is_weekend = false;
+// let is_sunny = true;
+// let is_weekend = false;
 
 
-console.log(is_sunny && is_weekend);
-console.log(is_sunny || is_weekend);
-console.log(!is_sunny)
-console.log(!is_weekend)
+// console.log(is_sunny && is_weekend);
+// console.log(is_sunny || is_weekend);
+// console.log(!is_sunny)
+// console.log(!is_weekend)
+
+
+let firstname =  "abdiqafaar"
+let lastname = "sooyaal" 
+
+console.log(firstname + "  " + lastname);
+
+
+let greating = "hello"
+
+
+greating += " " + "good morning"
+
+greating += firstname
+
+console.log(greating)
---
+++ exercise4/function.js
@@ -0,0 +1,22 @@
+
+
+// basic function
+
+function add(a,b){
+    console.log(a+b)
+}
+
+add(5,2)
+add(80,20)
+add(150,50)
+
+// function expressions
+
+const adding = function add2(c,d){
+    console.log(c+d)
+
+}
+
+add(40,80)
+add(20,80)
+add(70,80)
